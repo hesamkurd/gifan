f7f4172e94841463bc25f05240dee9a8a5f3a759
mamhesam <mamhesam1@gmail.com>
2023-05-31 20:10:40 +0330

    fix isuue

--- src/js/app.js
+++ src/js/app.js
@@ -5,7 +5,7 @@ import './../css/app.css'
 import { tooltip } from './tooltip'
 import { nav } from './nav'
 import { themeToggler } from './themeToggler'
-import { DropDown } from './dropDown'
+import { DropDown } from './dropdown'
 
 tooltip({ theme: 'dark' })
 nav()
